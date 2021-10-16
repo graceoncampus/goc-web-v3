ac92faf4f5891116dedfd45c950d00b4dd484a7c
Isaac Li <isaac.li@ucla.edu>
2021-10-16 15:51:56 -0700

    Add Isaac

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ const App = () => {
   return (
     <div className="webTeamMembers">
       <span> Web Team Members! </span>
-      <span> Ricky Guo :) </span> 
+      <span> Ricky Guo :) </span>
       <span> James Wang :) </span>
       <span> Jasper Edbrooke (: </span>
       <span> Daniel Branda :( </span>
+      <span> Isaac Li :0</span>
     </div>
   );
-}
+};
 
 export default App;
