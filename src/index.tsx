5a54b344cb16636396e8da38264f154567a3c6ee
Isaac Li <40645885+eyesaacli@users.noreply.github.com>
2022-02-12 15:30:46 -0800

    Add routing to website (#20)
    * Add routing to website to render thank you page and redirect to home
    page after rider signup form submission.
    
    Testing Steps
    ------------------------------------------------------------------------
    1. yarn && yarn build && yarn start
    2. navigate to localhost:3000
    3. click 'go to rider signup' button to navigate to rider signup form
    4. ensure that form is at /rides/rider/signup
    5. test form submission by pressing 'sign up' button at bottom of form
    6. ensure that thank you page is rendered at same url for 4 seconds
    7. ensure that you are redirected to home page at localhost:3000/
    
    Resources
    ------------------------------------------------------------------------
    - react-router: https://reactrouter.com
    
    * Add routing to rider signup form and landing page.
    
    Testing Steps
    ------------------------------------------------------------------------
    1. yarn && yarn build && yarn start
    2. navigate to localhost:3000
    3. press button to navigate to rider signup form
    4. ensure that signup form is at localhost:3000/rides/rider/signup
    
    Resources
    ------------------------------------------------------------------------
    - react-router: https://reactrouter.com/
    
    Co-authored-by: Isaac Li <isaac.li@ucla.edu>

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/bootstrap.scss';
 import App from './App'; // Keep temporarily or linter will complain
-
-import { RiderSignup } from "./Components/Rides/RiderSignup/RiderSignup";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RiderSignup } from './Components/Rides/RiderSignup/RiderSignup';
+import { Landing } from './Components/Landing/Landing';
 
 ReactDOM.render(
   <React.StrictMode>
-    <RiderSignup />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="rides/rider/signup" element={<RiderSignup />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
