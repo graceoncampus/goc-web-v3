e80e1597dc3d3e20f2e5b3ca51526b3f172b5ca3
Maggie Yan <maggie.yan414@gmail.com>
2022-02-14 09:26:06 -0800

    Revision: add .gitmessage to commit and fixed nits

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,25 +10,25 @@ import './Header.scss'
 export const Header = () => {
     return (
         <Container>
-            <img className='goc-header' src='./assets/goc-header.png' alt='GOC'></img>
-            <Row className='nav-bar'>
+            <img className={'goc-header'} src={'./assets/goc-header.png'} alt={'GOC'}></img>
+            <Row className={'nav-bar'}>
                 <Col>
-                    <a className={'nav-link'} href=''>About</a>
+                    <a className={'nav-link'} href={''}>About</a>
                 </Col>
                 <Col>
-                    <a className={'nav-link'} href=''>Resources</a>
+                    <a className={'nav-link'} href={''}>Resources</a>
                 </Col>
                 <Col xs={2}>
-                    <a className={'nav-link'} href=''>Small Groups</a>
+                    <a className={'nav-link'} href={''}>Small Groups</a>
                 </Col>
                 <Col>
-                    <a className={'nav-link'} href=''>Rides</a>
+                    <a className={'nav-link'} href={''}>Rides</a>
                 </Col>
                 <Col>
-                    <a className={'nav-link'} href=''>Login</a>
+                    <a className={'nav-link'} href={''}>Login</a>
                 </Col>
                 <Col>
-                    <Button variant='primary' className='account-sign-up-button'>
+                    <Button variant={'primary'} className={'account-sign-up-button'}>
                         SIGN UP
                     </Button>
                 </Col> 
