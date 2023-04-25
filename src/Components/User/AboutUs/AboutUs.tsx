96da6833e33402a76a8d515266f27eb64fffd188
Shawn Zhuang <cubershz@gmail.com>
2023-04-24 21:10:06 -0700

    Add banner

--- src/Components/User/AboutUs/AboutUs.tsx
+++ src/Components/User/AboutUs/AboutUs.tsx
@@ -3,6 +3,9 @@
  */
 
 import React, { useState } from 'react';
+import { Image } from "react-bootstrap";
+
+import './AboutUs.scss'
 
 import { Button } from 'react-bootstrap'
 
@@ -18,10 +21,11 @@ export const AboutUs = () => {
 const AboutUsBody = () => {
     return (
         <div className='text-center'>
-            <h1>About Us</h1>
+            <Image fluid rounded src={'/images/AboutUsBackground.png'}/>
+            <h1 className="aboutUs">About Us</h1>
             <h2>Who We Are</h2>
             <p>
-            We are a ministry of Grace Community Church on the UCLA campus. We are a student group that exists to glorify God and spread a passion for His glory by making disciples, shepherding them to value Jesus Christ above all else, and training up the next generation of Christian leaders. In other words, we exist to edify and equip the saints, evangelize the lost, and exalt the Lord Jesus Christ in all things.
+                We are a ministry of Grace Community Church on the UCLA campus. We are a student group that exists to glorify God and spread a passion for His glory by making disciples, shepherding them to value Jesus Christ above all else, and training up the next generation of Christian leaders. In other words, we exist to edify and equip the saints, evangelize the lost, and exalt the Lord Jesus Christ in all things.
             </p>
             <p>
             Every Friday night we gather for a time of singing, hearing God's Word taught, and fellowship. It is during these weekly large group meetings that GOC comes together corporately to worship our great God.
