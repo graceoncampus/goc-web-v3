a498b3584e165bd65f6d815d1e9c2f095b835bff
Shawn Zhuang <cubershz@gmail.com>
2023-04-24 21:16:53 -0700

    center text

--- src/Components/User/AboutUs/AboutUs.tsx
+++ src/Components/User/AboutUs/AboutUs.tsx
@@ -22,14 +22,18 @@ const AboutUsBody = () => {
     return (
         <div className='text-center'>
             <Image fluid rounded src={'/images/AboutUsBackground.png'}/>
-            <h1 className="aboutUs"> <strong> About Us </strong></h1>
+            <h1 className="about-us"> <strong> About Us </strong></h1>
             <h2>Who We Are</h2>
-            <p>
-                We are a ministry of Grace Community Church on the UCLA campus. We are a student group that exists to glorify God and spread a passion for His glory by making disciples, shepherding them to value Jesus Christ above all else, and training up the next generation of Christian leaders. In other words, we exist to edify and equip the saints, evangelize the lost, and exalt the Lord Jesus Christ in all things.
-            </p>
-            <p>
-            Every Friday night we gather for a time of singing, hearing God's Word taught, and fellowship. It is during these weekly large group meetings that GOC comes together corporately to worship our great God.
-            </p>
+            <div className="about-us-container">
+                <div className='about-us-body'>
+                    <p>
+                        We are a ministry of Grace Community Church on the UCLA campus. We are a student group that exists to glorify God and spread a passion for His glory by making disciples, shepherding them to value Jesus Christ above all else, and training up the next generation of Christian leaders. In other words, we exist to edify and equip the saints, evangelize the lost, and exalt the Lord Jesus Christ in all things.
+                    </p>
+                    <p>
+                        Every Friday night we gather for a time of singing, hearing God's Word taught, and fellowship. It is during these weekly large group meetings that GOC comes together corporately to worship our great God.
+                    </p>
+                </div>
+            </div>
             <Button variant='dark'>
                 Welcome Packet
             </Button>
