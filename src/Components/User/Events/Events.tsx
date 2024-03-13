a5f7953767d7f0c2ccf761c71a7e6038e996a7f6
Shawn Zhuang <cubershz@gmail.com>
2024-03-12 19:24:09 -0700

    feat: add events accordian

--- src/Components/User/Events/Events.tsx
+++ src/Components/User/Events/Events.tsx
@@ -2,6 +2,7 @@ import { HeaderNavbarActiveKey } from "Components/User/Header/Header";
 import { Template } from "Components/User/Template/Template";
 
 import "./Events.scss";
+import { Accordion } from "react-bootstrap";
 
 export const Events = () => {
   return (
@@ -18,6 +19,17 @@ const EventsBody = () => {
       <h1 className="events">
         <strong> Events </strong>
       </h1>
+      <h1 className="m-5">Upcoming Events</h1>
+      <div className="p-5 m-5 pt-0">
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+              <h2>Large group</h2>
+            </Accordion.Header>
+            <Accordion.Body>YAY</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
     </div>
   );
 };
