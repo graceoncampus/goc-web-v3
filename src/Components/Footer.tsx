d1acbd696e8ba6c2b8308f9b5a91aa85b9a2482b
Shawn Zhuang <cubershz@gmail.com>
2024-12-04 20:45:41 -0800

    Footer finish

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@
  * Footer.
  */
 
-import { Box, Center, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Image, Input, Text } from "@chakra-ui/react";
 import React from "react";
 
 export interface FooterColumnProps {
@@ -32,19 +32,28 @@ export default function Footer() {
       <FooterColumn title="GRACE ON CAMPUS">
         <Text>Fridays at 7pm</Text>
         <Text>Broad Art Center 2160E</Text>
-        <Text>CONTACT US</Text>
+      </FooterColumn>
+      <FooterColumn title="Contact Us">
         <Text>Phillip Ko</Text>
         <Text>(510) 612-7862</Text>
         <Text>gocateam@gmail.com</Text>
       </FooterColumn>
-      <FooterColumn title="NEW TO GOC?">hey</FooterColumn>
-      <FooterColumn title="Contact Us">hey</FooterColumn>
+      <FooterColumn title="NEW TO GOC?">
+        <Text>We'd love to get in touch with you!</Text>
+        <Text fontWeight={"bold"}>Name: </Text>
+        <Input />
+        <Text fontWeight={"bold"}>Email: </Text>
+        <Input />
+        <Button marginTop={"4px"} background={"goc.blue"}>
+          Submit
+        </Button>
+      </FooterColumn>
       <FooterColumn title="FOLLOW US">
-        <Flex>
-          <Image height={"35px"} src={"/assets/logo-facebook.png"} alt={"Facebook"} />
-          <Image height={"35px"} src={"/assets/logo-instagram.png"} alt={"Instagram"} />
-          <Image height={"35px"} src={"/assets/logo-vimeo.png"} alt={"Vimeo"} />
-          <Image height={"35px"} src={"/assets/logo-wordpress.png"} alt={"Wordpress"} />
+        <Flex justify={"space-between"}>
+          <Image height={"35px"} src={"/assets/logo-facebook.png"} alt={"Facebook"} margin={"0 0.5rem"} />
+          <Image height={"35px"} src={"/assets/logo-instagram.png"} alt={"Instagram"} margin={"0 0.5rem"} />
+          <Image height={"35px"} src={"/assets/logo-vimeo.png"} alt={"Vimeo"} margin={"0 0.5rem"} />
+          <Image height={"35px"} src={"/assets/logo-wordpress.png"} alt={"Wordpress"} margin={"0 0.5rem"} />
         </Flex>
       </FooterColumn>
     </Flex>
