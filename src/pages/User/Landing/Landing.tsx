e858af5969fadb4c6d7eeda73fedc9962058c1f4
Yeadam Kim <yes752044@gmail.com>
2024-12-04 22:12:52 -0800

    Progress on landing page

--- src/pages/User/Landing/Landing.tsx
+++ src/pages/User/Landing/Landing.tsx
@@ -9,8 +9,8 @@ import { Template } from "pages/User/Template/Template";
 import { EventCardList, EventCardListProps } from "pages/EventCardList/EventCardList";
 
 import { observer } from "mobx-react-lite";
-import { Button, Row } from "react-bootstrap";
 import { useStore } from "store/StoreContext";
+import { Box, Button, Center, Flex, Heading, Image, Link, Stack, StackSeparator, Text } from "@chakra-ui/react";
 
 const LandingBody = observer(() => {
   const navigate = useNavigate();
@@ -54,35 +54,67 @@ const LandingBody = observer(() => {
   ];
 
   return (
-    <div className={"landing-container"}>
-      <div className={"landing-rounded-blue-box"}>
-        <div className={"landing-welcome-title"}>Welcome to Grace on Campus!</div>
-        <div className={"landing-welcome-subtitle"}>Fridays at 7pm, Rolfe 1200</div>
-        <a className={"landing-facebook-btn"} href={"https://www.facebook.com/groups/gocatucla"} target={"_blank"}>
-          Join our Facebook group
-        </a>
-      </div>
-      <div className={"landing-welcome-image-container"}>
-        <img className={"landing-welcome-image"} src="/images/LandingPhoto.png" alt="Grace on Campus" />
-      </div>
-      <div className={"landing-welcome-paragraph-container"}>
-        <div className={"landing-welcome-paragraph-title"}>Welcome!</div>
-        <div className={"landing-welcome-paragraph-body"}>
-          Grace on Campus is a Christian fellowship at UCLA. As a ministry of{" "}
-          <a target={"_blank"} href={"https://www.gracechurch.org/"}>
-            Grace Community Church
-          </a>
-          , we focus on Bible teaching, leadership training, evangelism, and discipleship. We also provide{" "}
-          <a href={"/rides/rider/signup"}>rides</a> to church every Sunday! Sign up for a ride to church and join our
-          Facebook group to attend our weekly Bible study, every Friday at 7pm.
-        </div>
-        <Row className={"btn-container"}>
-          <Button onClick={() => navigate("/about")}>About Us</Button>
-          <Button onClick={() => navigate("/ourbeliefs")}>What we believe</Button>
-        </Row>
-      </div>
+    <Box>
+      <Stack>
+        <Box position="relative" width="100vw" height={["50vh", "60vh", "75vh"]}>
+          <Image width="100%" height="100%" src="/images/landing.jpg" alt="GOC Landing Photo" objectFit="cover" />
+          <Box position="absolute" top={0} left={0} width="100%" height="100%" bg="gray.900" opacity={0.5} />
+          <Center position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)">
+            <Stack textAlign="center" align="center" color="white" px={4}>
+              <Heading size={["2xl", "3xl", "5xl"]} fontWeight="bold">
+                Welcome to Grace on Campus!
+              </Heading>
+              <Text fontSize={["md", "lg", "2xl"]}>Fridays at 7pm, Rolfe 1200</Text>
+              <Link href={"https://www.facebook.com/groups/gocatucla"} target={"_blank"}>
+                <Button
+                  variant="outline"
+                  borderColor="white"
+                  color="white"
+                  _hover={{ bg: "white", color: "black" }}
+                  size="lg"
+                  borderRadius="full"
+                  paddingX={6}
+                  paddingY={4}
+                >
+                  Join our Facebook group
+                </Button>
+              </Link>
+            </Stack>
+          </Center>
+        </Box>
+      </Stack>
+
+      <Stack direction={{ base: "column", md: "row" }} h="auto" w="100%" align="stretch">
+        <Box flex={1} textAlign="center" p={4} bg="gray.100" borderRadius="md">
+          <Heading size="md" mb={2}>
+            New Here?
+          </Heading>
+          <Text mb={4}>Learn more about GOC</Text>
+          <Button variant="outline" width="full">
+            ABOUT US
+          </Button>
+        </Box>
+        <Box flex={1} textAlign="center" p={4} bg="gray.100" borderRadius="md">
+          <Heading size="md" mb={2}>
+            Small Group
+          </Heading>
+          <Text mb={4}>Get plugged in!</Text>
+          <Button variant="outline" width="full">
+            LEARN MORE
+          </Button>
+        </Box>
+        <Box flex={1} textAlign="center" p={4} bg="gray.100" borderRadius="md">
+          <Heading size="md" mb={2}>
+            Upcoming Events
+          </Heading>
+          <Text mb={4}>Join us for upcoming happenings</Text>
+          <Button variant="outline" width="full">
+            LEARN MORE
+          </Button>
+        </Box>
+      </Stack>
       <EventCardList events={events} />
-    </div>
+    </Box>
   );
 });
 
