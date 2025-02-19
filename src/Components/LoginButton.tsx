52190c94e179efcb7ad529010ed0203b44d9aebf
Yeadam Kim <yes752044@gmail.com>
2025-02-19 02:24:01 -0800

    UI Improvements on LoginButton

--- src/Components/LoginButton.tsx
+++ src/Components/LoginButton.tsx
@@ -1,6 +1,14 @@
 import { Button, Link } from "@chakra-ui/react";
+import {
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from "@/components/ui/menu";
 import { getCurrentUser, signOut } from "aws-amplify/auth";
 import { useEffect, useState } from "react";
+import { FaUser } from "react-icons/fa6";
+import { MdLogout } from "react-icons/md";
 
 const LoginButton = () => {
   const [user, setUser] = useState<string | null>(null);
@@ -18,43 +26,88 @@ const LoginButton = () => {
   }, []);
 
   return (
-    <Button
-      asChild={true}
-      position="relative"
-      outline="none"
-      border="none"
-      margin=".5rem"
-      color="white"
-      fontWeight="bold"
-      boxShadow="none"
-      backgroundColor="goc.blue"
-      borderRadius="1.2rem"
-      transition="background .2s ease, box-shadow .2s ease"
-      _hover={{
-        textDecoration: "none",
-        boxShadow: "md",
-        backgroundColor: "rgb(48, 90, 175)", // slightly darker blue than goc.blue
-        transition: "background 50ms ease, box-shadow 50ms ease",
-      }}
-      _focusVisible={{
-        outline: "2px solid black",
-        outlineOffset: "2px",
-      }}
-    >
+    <>
       {user ? (
-        <Link
-          onClick={async (e) => {
-            e.preventDefault();
-            await signOut();
-            window.location.reload();
+        <MenuRoot positioning={{ placement: "bottom-end" }}>
+          <MenuTrigger asChild>
+            <Button
+              outline="none"
+              border="none"
+              margin=".5rem"
+              color="white"
+              fontWeight="bold"
+              backgroundColor="goc.blue"
+              borderRadius="1.2rem"
+              transition="background .2s ease, box-shadow .2s ease"
+              _hover={{
+                textDecoration: "none",
+                boxShadow: "md",
+                backgroundColor: "rgb(48, 90, 175)", // slightly darker blue than goc.blue
+                transition: "background 50ms ease, box-shadow 50ms ease",
+              }}
+              _focusVisible={{
+                outline: "2px solid black",
+                outlineOffset: "2px",
+              }}
+            >
+              {user}
+            </Button>
+          </MenuTrigger>
+          <MenuContent
+            backgroundColor="white"
+            boxShadow="md"
+            rounded="md"
+            padding={".25rem"}
+            marginTop={".5rem"}
+          >
+            <MenuItem asChild value="Profile">
+              <Link href="/profile">
+                <FaUser />
+                Profile
+              </Link>
+            </MenuItem>
+            <MenuItem asChild value="Log out">
+              <Link
+                onClick={async (e) => {
+                  e.preventDefault();
+                  await signOut();
+                  window.location.reload();
+                }}
+              >
+                <MdLogout />
+                Log out
+              </Link>
+            </MenuItem>
+          </MenuContent>
+        </MenuRoot>
+      ) : (
+        <Button
+          asChild={true}
+          position="relative"
+          outline="none"
+          border="none"
+          margin=".5rem"
+          color="white"
+          fontWeight="bold"
+          boxShadow="none"
+          backgroundColor="goc.blue"
+          borderRadius="1.2rem"
+          transition="background .2s ease, box-shadow .2s ease"
+          _hover={{
+            textDecoration: "none",
+            boxShadow: "md",
+            backgroundColor: "rgb(48, 90, 175)", // slightly darker blue than goc.blue
+            transition: "background 50ms ease, box-shadow 50ms ease",
+          }}
+          _focusVisible={{
+            outline: "2px solid black",
+            outlineOffset: "2px",
           }}
         >
-          Log out
-        </Link>
-      ) : (
-        <Link href="/login">Log In</Link>
+          <Link href="/login">Log In</Link>
+        </Button>
       )}
-    </Button>
+    </>
   );
 };
 
