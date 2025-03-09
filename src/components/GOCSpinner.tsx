08ece5fa60a985ab028438bd2e2b0c684d947f25
Yeadam Kim <yeadamkk@gmail.com>
2025-03-09 00:43:26 -0800

    Make GOCSpinner more customizable

--- src/components/GOCSpinner.tsx
+++ src/components/GOCSpinner.tsx
@@ -1,10 +1,18 @@
 import { Stack, Spinner, Text } from "@chakra-ui/react";
 
-const LoadingSpinner: React.FC = () => {
+interface LoadingSpinnerProps {
+  text?: string;
+  marginY?: string;
+}
+
+const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
+  text = "Loading...",
+  marginY = "1rem",
+}) => {
   return (
-    <Stack marginY="1rem" align="center">
+    <Stack marginY={marginY} align="center">
       <Spinner color="goc.blue" animationDuration="0.8s" borderWidth="3px" />
-      <Text color="goc.blue">Loading...</Text>
+      <Text color="goc.blue">{text}</Text>
     </Stack>
   );
 };
