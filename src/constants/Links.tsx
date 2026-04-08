1fd43ccdae9de3811eb5b6404b6494d938a29937
Bryan Yu <54421861+lelinyu@users.noreply.github.com>
2026-04-08 12:59:31 -0700

    Update back to normal rides link in Links.tsx

--- src/constants/Links.tsx
+++ src/constants/Links.tsx
@@ -19,7 +19,7 @@ export const SocialMedia: SocialMediaProps = {
 };
 
 export const RIDES_GOOGLE_FORM_LINK: string =
-  "https://docs.google.com/spreadsheets/d/1Pq8RfmSOVA68fa1ytF4RwPHCHxvM_d_NQ-owQjmM98I/edit?gid=1708287295#gid=1708287295";
+  "https://docs.google.com/forms/d/1wNbm8h3jFj5wS6RlSn52OCdl8kvof9I_nULmv0dnNSo/viewform?edit_requested=true";
 
 export const PRAYER_GOOGLE_FORM_LINK: string =
   "https://docs.google.com/forms/d/e/1FAIpQLSc0PR3d6NL1YqWMoYZ9X1Y9aBN4y91FfXz152I2_h7HQaHAwA/viewform?usp=dialog";
